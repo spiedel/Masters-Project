a638a58d8e8cb9640bda804a1c2858cabf328bec
spiedel <leah@spiedel.co.uk>
2020-02-12 16:18:55 +0000

    More plotting of contours with new code

--- ex2/chi2.py
+++ ex2/chi2.py
@@ -22,10 +22,14 @@ def chi2_local(observed, expected, error):
 
 def getPred(fh, wcValue1, wcValue2):
 	#funtion for predicting the theory values for a given pair of wilson coefficients
+	#{'qq3_i33i', 'G', 'qq1_ii33', 'uu_i33i', 'qu1_33ii', 'qu8_33ii', 'qq1_i33i', 'qu8_ii33', 'qq3_ii33', 'uG_33', 'ud1_33ii', 'qd8_33ii', 'qu1_ii33', 'uu_ii33', 'qd1_33ii', 'ud8_33ii'}
 	#returns
-	prediction = fh.predict(G=wcValue1, uG_33=wcValue2)#qq3_i33i=wcValue2)
+	prediction = fh.predict(qq3_i33i=wcValue1, qq1_i33i=wcValue2)#iqq3_i33i=wcValue2)#, uG_33=wcValue2)#
 	return [x[0] for x in prediction.values]
 
+
+wilco1 = "qq3\_i33i"
+wilco2 = "qq1\_i33i"
 
 #generate prediction array from data
 #af = AnalysisFrame.from_hdf('/nfs/topfitter/sbrown/HDF/analyses/ATLAS_2017_I1604029/ATLAS_2017_I1604029.h5')
@@ -38,7 +42,7 @@ obs = [x[0] for x in fh.reference.values]
 err = [x[1] for x in fh.reference.values]
 print(err)
 
-#print(pa.xr.obs)
+print(pa.wilcos)
 #print(pa.reference)
 #print(fh.reference)
 #print(pa.atpoint(G=1, qq3_i33i=1))
@@ -48,9 +52,9 @@ print(err)
 
  
 #plotting noValues*noValues with given x and y ranges
-noValues = 30
-xBins = np.linspace(-5, 5, noValues)
-yBins = np.linspace(-2, 2, noValues)
+noValues = 40
+xBins = np.linspace(-3, 3, noValues)
+yBins = np.linspace(-3, 3, noValues)
 
 i = 0
 j = 0
@@ -63,9 +67,9 @@ for x in xBins:
 		j+=1
 	j=0
 	i+=1
-print(pred)
+#print(pred)
 
-print(chi2Array)
+#print(chi2Array)
 #Calculate delta chi2
 
 chi2Array = chi2Array - chi2Array.min()
@@ -73,8 +77,8 @@ chi2Array = chi2Array - chi2Array.min()
 #calculate p
 pArray = chi2.sf(chi2Array, 2)
 
-print(chi2Array)
-print(pArray)
+#print(chi2Array)
+#print(pArray)
 
 #plot
 fig, ax = plt.subplots()
@@ -82,8 +86,7 @@ fig, ax = plt.subplots()
 #cont = ax.contourf(xBins, yBins, chi2Array)
 cont = ax.contourf(xBins, yBins, pArray, [0.01, 0.05, 0.35, 1.0]) 
 fig.colorbar(cont, ax=ax)
-
+plt.xlabel(wilco1)
+plt.ylabel(wilco2)
 plt.show()
-fig.savefig("contour_plots/contours_p_{}.png".format(noValues))
-
-
+fig.savefig("contour_plots/contours_p_{}_{}.png".format(wilco1.replace("\\",""), wilco2.replace("\\","")))
